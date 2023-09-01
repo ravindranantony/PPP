6890b2a37897c349e17ee7cb067555ae9759266c
MADHAN KARTHICK MAILSAMY <madhan1787@gmail.com>
2023-09-01 16:11:17 +0530

    new file

--- Madhan/tasks/Calculator/calculator.js
+++ Madhan/tasks/Calculator/calculator.js
@@ -1,17 +1,17 @@
-function display(value) {
-    document.getElementById("result").value += value;
-}
-
-function clearScreen() {
-    document.getElementById("result").value = "";
-}
-
-function calculate() {
-    try {
-        document.getElementById("result").value = 
-        eval(document.getElementById("result").value);
-    }
-    catch(err) {
-        document.getElementById("result").value = "Error";
-    }
-}
+function Solve(val) {
+    var v = document.getElementById('res');
+    v.value += val;
+  }
+  function Result() {
+    var num1 = document.getElementById('res').value;
+    var num2 = eval(num1);
+    document.getElementById('res').value = num2;
+  }
+  function Clear() {
+    var inp = document.getElementById('res');
+    inp.value = '';
+  }
+  function Back() {
+    var ev = document.getElementById('res');
+    ev.value = ev.value.slice(0,-1);
+  }
