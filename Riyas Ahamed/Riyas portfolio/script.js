e01fe25566323c01fcbcd37464d438292ab27997
Riyas ahamed J <riyasahamedj03@gmail.com>
2023-09-04 22:35:48 +0530

    Portfolio Update

--- Riyas Ahamed/Riyas portfolio/script.js
+++ Riyas Ahamed/Riyas portfolio/script.js
@@ -1,2 +1,29 @@
+// JavaScript to handle the sticky navbar
+window.addEventListener("scroll", () => {
+    const navbar = document.querySelector("header");
+    if (window.scrollY > 60) {
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+    }
+});
+document.addEventListener("DOMContentLoaded", function () {
+    const links = document.querySelectorAll("nav a");
+
+    links.forEach((link) => {
+        link.addEventListener("click", (event) => {
+            event.preventDefault();
+            const targetId = link.getAttribute("href").substring(1);
+            const targetSection = document.getElementById(targetId);
+
+            if (targetSection) {
+                window.scrollTo({
+                    top: targetSection.offsetTop,
+                    behavior: "smooth",
+                });
+            }
+        });
+    });
+});
 
     
