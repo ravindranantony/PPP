18ac14dd72c010571b2f47eed6d831075e38dac5
Sindhu Laheri <sindhulaheri1392@gmail.com>
2023-09-14 18:23:35 -0400

    PPP018 - Modification in Profile Page

--- SindhuLaheri/Tasks/Tic Tac Toe/ttt.js
+++ SindhuLaheri/Tasks/Tic Tac Toe/ttt.js
@@ -1,72 +1,131 @@
 const board = document.getElementById('board');
         const message = document.getElementById('message');
-        let currentPlayer = 'X';
-        let gameOver = false;
-
-        // Create the cells
+        const cells = [];
+        let currentPlayer = '💙';
+        let winner = null;
+        let isComputerTurn = false;
+      
+        // Create the Tic-Tac-Toe grid
         for (let i = 0; i < 9; i++) {
             const cell = document.createElement('div');
-            cell.className = 'cell';
+            cell.classList.add('cell');
             cell.dataset.index = i;
+            cell.addEventListener('click', handleCellClick);
+            cells.push(cell);
             board.appendChild(cell);
         }
 
         // Handle cell clicks
-        board.addEventListener('click', (e) => {
-            if (gameOver) return;
-            
-            const cell = e.target;
-            if (cell.classList.contains('cell') && !cell.textContent) {
-                cell.textContent = currentPlayer;
-                checkWinner();
-                currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-                message.textContent = currentPlayer === 'X' ? "Your turn" : "Computer's turn";
-                
-                // Simulate the computer's move (you can replace this with your AI logic)
-                setTimeout(computerMove, 500);
+        function handleCellClick(event) {
+            const cell = event.target;
+            const index = cell.dataset.index;
+
+            if (cells[index].textContent || winner || isComputerTurn) {
+                return;
             }
-        });
+
+            cells[index].textContent = currentPlayer;
+            currentPlayer = currentPlayer === '💙' ? '🔴' : '💙';
+
+            checkWinner();
+            
+            if (!winner) {
+                setTimeout(computerMove, 500); // Introduce a delay for the computer's move
+            }
+        }
 
         // Check for a winner
         function checkWinner() {
-            const cells = Array.from(document.querySelectorAll('.cell'));
-            const winningCombos = [
-                [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
-                [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
-                [0, 4, 8], [2, 4, 6] // Diagonals
+            const winningCombinations = [
+                [0, 1, 2], [3, 4, 5], [6, 7, 8],
+                [0, 3, 6], [1, 4, 7], [2, 5, 8],
+                [0, 4, 8], [2, 4, 6]
             ];
 
-            for (const combo of winningCombos) {
-                const [a, b, c] = combo;
+            for (const combination of winningCombinations) {
+                const [a, b, c] = combination;
                 if (cells[a].textContent && cells[a].textContent === cells[b].textContent && cells[a].textContent === cells[c].textContent) {
-                    message.textContent = `${currentPlayer === 'X' ? "You" : "Computer"} wins!`;
-                    gameOver = true;
+                    cells[a].style.backgroundColor = 'green';
+                    cells[b].style.backgroundColor = 'green';
+                    cells[c].style.backgroundColor = 'green';
+                    winner = cells[a].textContent;
+                    message.textContent = `${winner} wins!`;
+                    
                     return;
                 }
             }
 
-            if (cells.every(cell => cell.textContent)) {
+            if (![...cells].some(cell => cell.textContent === '')) {
                 message.textContent = "It's a draw!";
-                gameOver = true;
+                
+                    return;
             }
         }
-
-        // Simulate the computer's move (random move)
-        function computerMove() {
-            const emptyCells = Array.from(document.querySelectorAll('.cell')).filter(cell => !cell.textContent);
-            if (emptyCells.length > 0 && !gameOver) {
-                const randomIndex = Math.floor(Math.random() * emptyCells.length);
-                emptyCells[randomIndex].textContent = currentPlayer;
-                checkWinner();
-                currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-                message.textContent = "Your turn";
+        
+        
+        // Computer's move (random)
+        
+        function computerMove()
+        {
+        const emptyCells = cells.filter(cell => !cell.textContent);
+            if (emptyCells.length === 0 || winner) {
+                return;
             }
+
+        const winningCombinations = [
+                [0, 1, 2], [3, 4, 5], [6, 7, 8],
+                [0, 3, 6], [1, 4, 7], [2, 5, 8],
+                [0, 4, 8], [2, 4, 6]
+            ];
+
+            for (const combination of winningCombinations) {
+                const [a, b, c] = combination;
+                if (cells[a].textContent && cells[a].textContent === cells[b].textContent) {
+                if (!cells[c].textContent)
+                {
+                cells[c].textContent = currentPlayer;
+                currentPlayer = currentPlayer === '💙' ? '🔴' : '💙';
+
+            checkWinner();
+            return;
+                }
+                }
+                
+                if (cells[b].textContent && cells[b].textContent === cells[c].textContent) {
+                if (!cells[a].textContent)
+                {
+                cells[a].textContent = currentPlayer;
+                currentPlayer = currentPlayer === '💙' ? '🔴' : '💙';
+
+            checkWinner();
+            return;
+                }
+                }
+                
+                if (cells[c].textContent && cells[c].textContent === cells[a].textContent) {
+                if (!cells[b].textContent)
+                {
+                cells[b].textContent = currentPlayer;
+                currentPlayer = currentPlayer === '💙' ? '🔴' : '💙';
+
+            checkWinner();
+            return;
+                }
+                }
+                }
+       
+            const randomIndex = Math.floor(Math.random() * emptyCells.length);
+            const randomCell = emptyCells[randomIndex];
+            randomCell.textContent = currentPlayer;
+            currentPlayer = currentPlayer === '💙' ? '🔴' : '💙';
+
+            checkWinner();
         }
         function restartGame() {
-            currentPlayer = 'X';
+            currentPlayer = '💙';
             gameBoard = ['', '', '', '', '', '', '', '', ''];
             gameActive = true;
-            document.getElementById('status').innerText = "Player X's turn";
+            document.getElementById('status').innerText = "Player 💙's turn";
         
             const cells = document.getElementsByClassName('cell');
             for (let i = 0; i < cells.length; i++) {
@@ -74,5 +133,6 @@ const board = document.getElementById('board');
             }
         }
         
-        document.getElementById('status').innerText = "Player X's turn";
+        document.getElementById('status').innerText = "Player 💙's turn";
         
+    
